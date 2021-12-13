b57bf0edca575bef425fdd448771ae0b2a2d62ee
Gabi <ghalas827@gmail.com>
2021-12-13 08:18:33 +0100

    13.12

--- app/src/main/java/com/example/sugar/addSugar.kt
+++ app/src/main/java/com/example/sugar/addSugar.kt
@@ -5,11 +5,15 @@ import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EdgeEffect
 import android.widget.EditText
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_add_sugar.*
 import kotlinx.android.synthetic.main.fragment_add_sugar.view.*
+import kotlinx.android.synthetic.main.fragment_home_screen.view.*
+import kotlinx.android.synthetic.main.fragment_sugar_ok.view.*
 import java.util.*
 
 
@@ -24,16 +28,32 @@ class addSugar : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_add_sugar, container, false)
         getView()?.findViewById<EditText>(R.id.addsugarEdittext)
-        val wynik = addsugarEdittext.text.toString()
-        if(wynik.toFloat()>0)
-        {}
+
+        /*getView()?.findViewById<Button>(R.id.add_button)?.setOnClickListener {
+            var string = addsugarEdittext.text.toString()
+            if(string.toFloat()>5)
+            {
+                view.ok_button.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_addSugar_to_sugarOk) }
+            }
+            else{
+                view.ok_button.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_addSugar_to_sugarHigh) }
+            }
+        }*/
+        view.ok_button.setOnClickListener {
+            var string = addsugarEdittext.text.toString()
+            if(string.toFloat()>5)
+            {
+                view.ok_button.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_addSugar_to_sugarOk) }
+            }
+            else{
+                view.ok_button.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_addSugar_to_sugarHigh) }
+            } }
 
 
         return view
-    }
+        }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
 
-    }
+
+
 }
