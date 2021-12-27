07bbad1e101f51b9e9da7744a59335aea34c7f7a
Gabi <ghalas827@gmail.com>
2021-12-27 15:46:21 +0100

    logowanie

--- app/src/main/java/com/example/sugar/LogIn.kt
+++ app/src/main/java/com/example/sugar/LogIn.kt
@@ -6,20 +6,47 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_add_sugar.view.*
+import kotlinx.android.synthetic.main.fragment_log_in.*
 import kotlinx.android.synthetic.main.fragment_log_in.view.*
 
 class LogIn : Fragment() {
-
+    private val fbAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_log_in, container, false)
-        view.buttonLogOK.setOnClickListener { Navigation.findNavController(view).navigate((R.id.action_logIn_to_homeScreen)) }
+
+        if(view.buttonLogOK!=null)
+        {
+            view.buttonLogOK.setOnClickListener {
+                setupLoginClick(view)
+            }
+        }
         view.goToRegistration.setOnClickListener { Navigation.findNavController(view).navigate((R.id.action_logIn_to_registration)) }
         view.forgotpassword.setOnClickListener { Navigation.findNavController(view).navigate((R.id.action_logIn_to_forgotPassword)) }
-
         return view
+    }
+
+    private fun setupLoginClick(view: View) {
+        //logowanie
+        runCatching  {
+            val email: String = emailLogIn.text?.trim().toString()
+            val password: String = passwordLogIn.text?.trim().toString()
+            fbAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener {
+                    Navigation.findNavController(view).navigate((R.id.action_logIn_to_homeScreen))
+                }
+        }.onFailure {
+             Navigation.findNavController(view).navigate((R.id.action_logIn_to_logInError))
+
+        }
+
     }
 
 }
--- app/src/main/java/com/example/sugar/registration.kt
+++ app/src/main/java/com/example/sugar/registration.kt
@@ -6,10 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_log_in.view.*
 import kotlinx.android.synthetic.main.fragment_registration.view.*
 
 class registration : Fragment() {
+    private val fbAuth: FirebaseAuth = FirebaseAuth.getInstance()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,8 +23,11 @@ class registration : Fragment() {
         val view =inflater.inflate(R.layout.fragment_registration, container, false)
         view.confirmRegistration.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_registration_to_homeScreen) }
 
+
         return view
     }
+
+
 
 
 }
